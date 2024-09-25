480a3dcdc0e71a4d362a95bcd3010093a90fc7b6
goel-anubhav <anubhavgoyal12062001@gmail.com>
2024-09-25 14:35:40 +0530

    Updated Contact Information

--- utils/data/contactsData.js
+++ utils/data/contactsData.js
@@ -1,7 +1,7 @@
 export const contactsData = {
   email: "anubhavgoyal12062001@gmail.com",
   phone: "+91-9355079966",
-  address: "Noida, IN",
+  address: "Noida, India",
   github: "https://github.com/goel-anubhav",
   facebook: "https://www.facebook.com/anubhav.goyal.121",
   linkedIn: "https://www.linkedin.com/in/anubhav-goel-1206/",
